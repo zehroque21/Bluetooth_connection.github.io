65489be5a9af43455eb86d96de7ecf31dc919fac
Amado Jose <muja.dr21@gmail.com>
2018-06-04 11:10:25 -0300

    test a button event

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ console.log("Hello");
 
 document.getElementById('Hi').innerHTML = 'oioi';
 
-button.addEventListener('pointerup', function(event) {
+event.addEventListener('pointerup', function(event) {
     // Call navigator.bluetooth.requestDevice
   });
