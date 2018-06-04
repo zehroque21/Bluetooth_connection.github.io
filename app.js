ac9e2f7ed5a25567a2f44d5ae521ff153195ad48
Amado Jose <muja.dr21@gmail.com>
2018-06-04 11:31:00 -0300

    changes and changes

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ var button = document.getElementById('button')
 button.addEventListener('pointerup', function(event) {
     // Call navigator.bluetooth.requestDevice
   });
+
+function event(){
+    document.getElementById('Hi').innerHTML = 'you can click'
+}
